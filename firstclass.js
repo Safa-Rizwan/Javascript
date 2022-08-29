fcf4f24c726b30e8c2861d604d179ce0c27307db
Safa Rizwan <103735300+Safa-Rizwan@users.noreply.github.com>
2022-08-29 10:35:15 +0500

    1st commit

---
+++ firstclass.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+// console.log gives result/output on console;
+console.log(2+3);
+console.log("My name is Safa Rizwan");
+// console.log = print/output
+// Variables = containers that store your assign values
+// 456
+// var is used for making containers
+var number = 456;
+console.log(number);
+var fruit= "Mango";
+// console.log(fruit);
+var fruit = "Apple";
+console.log(fruit);
+// Only latest values will store in variable, previous one will be override.
+var a= 100;
+var b= 332;
+var c= a + b;
+// 100 + 332 = 432 -----> c
+console.log(c);
+console.log("The value store in Variable c is",c);
+var d= b-a;
+console.log("The value store in variable d is",d);
+// 332-100=232 -----> d
+// var, cons, let
+
